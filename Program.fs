dbf2ba6c97b8fdbeed6775045a75da0aad59bf1a
odininon <mike.stengel@gmail.com>
2013-10-14 03:26:43 -0400

    Finds all blocks and items. Adds them to a mods dictionary

--- Program.fs
+++ Program.fs
@@ -7,6 +7,7 @@ module ConfigHelper.Main
 
 open System
 open System.IO
+open System.Collections.Generic
 
 let readFile (filePath:string) = seq {
     use sr = new StreamReader (filePath)
@@ -21,12 +22,43 @@ let removeEmptyLines = seqFilter (fun (x:string) -> not (x.Equals ""))
 
 let trimSeq (sequence:seq<string>) = sequence |> removeComments |> removeEmptyLines
 
+let getBlock (s:string) (sq:seq<string>) = 
+    let index = sq |> Seq.findIndex (fun (x:string) -> x.Equals (s + " {"))
+    let newsq = sq |> Seq.skip (index + 1)
+    let indexEnd = newsq |> Seq.findIndex (fun (x:string) -> x.Equals "}")
+    newsq |> Seq.take indexEnd 
+    
+let getValuesFromString (s : string) =
+    let index = s.IndexOf ":"
+    let cleanString = s.Substring (index + 1) 
+    let values = cleanString.Split [| '=' |]
+    values
+    
+let getDictionaryFromSeq sq =
+    let dict = Dictionary<string,string>()
+    sq |> Seq.iter (fun (x: string) -> dict.Add((getValuesFromString x).[0], (getValuesFromString x).[1]))
+    dict
+    
+let getNameFromFile (f:string) = (f.Split [| '.' |]).[0]
+    
+let printCollection targetSet =
+    targetSet |> Seq.iter (fun x -> printf "%O" x)
+    
 [<EntryPoint>]
 let main args = 
-    let file = readFile "BiblioCraft.cfg"
+    let filename = "BiblioCraft.cfg"
+    let file = readFile filename
     
     let trimmedFile = trimSeq file
+        
+    let blocks = getBlock "block" trimmedFile
+    let items = getBlock "item" trimmedFile
     
-    trimmedFile |> Seq.iter (fun elem -> printfn "%s" elem)
+    let modDict = new Dictionary<string,Dictionary<string,string>>()
+    modDict.Add((getNameFromFile filename) + " blocks", getDictionaryFromSeq blocks)    
+    modDict.Add((getNameFromFile filename) + " items", getDictionaryFromSeq items)    
+                            
+    printCollection modDict
+
     0
 
